295111f15ce5619665f75ead74a322e02a82cdba
andrey.plakhotin <andrey.plakhotin@softswiss.com>
2023-12-17 11:24:40 +0300

    1. Visibility. Общий ресурс вне критической секции fix

--- src/main/java/ru/job4j/io/ParseFile.java
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -11,17 +11,17 @@ public class ParseFile {
     }
 
     public String getContent(Predicate<Character> filter) {
-        String output = "";
+        StringBuilder output = new StringBuilder();
         try (BufferedInputStream input = new BufferedInputStream(new FileInputStream(file))) {
             int data;
-            while ((data = input.read()) > 0) {
+            while ((data = input.read()) != 0) {
                 if (filter.test((char) data)) {
-                    output += (char) data;
+                    output.append((char) data);
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return output;
+        return output.toString();
     }
 }
