f37b2339d977e615318a0eb64e155a1adc347f53
andrey.plakhotin <andrey.plakhotin@softswiss.com>
2024-02-10 14:18:58 +0300

    3. ForkJoinPool fix

--- src/main/java/ru/job4j/pool/ParallelSearch.java
+++ src/main/java/ru/job4j/pool/ParallelSearch.java
@@ -1,19 +1,14 @@
 package ru.job4j.pool;
 
-import java.util.List;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
 public class ParallelSearch<T> extends RecursiveTask<Integer> {
 
-    private T[] objects;
-    private T object;
-    private int from;
-    private int to;
-
-    public ParallelSearch() {
-
-    }
+    private final T[] objects;
+    private final T object;
+    private final int from;
+    private final int to;
 
     private ParallelSearch(T[] objects, T object, int from, int to) {
         this.objects = objects;
@@ -25,7 +20,7 @@ public class ParallelSearch<T> extends RecursiveTask<Integer> {
     @Override
     protected Integer compute() {
         if (to - from <= 10) {
-            return List.of(objects).indexOf(object);
+            return linearSearch(objects, object, from, to);
         }
         int middle = (from + to) / 2;
         ParallelSearch<T> leftSearch = new ParallelSearch<>(objects, object, from, middle);
@@ -34,10 +29,21 @@ public class ParallelSearch<T> extends RecursiveTask<Integer> {
         rightSearch.fork();
         int left = leftSearch.join();
         int right = rightSearch.join();
-        return left >= 0 ? left : right;
+        return Math.max(left, right);
     }
 
-    public int search(T[] objects, T object) {
+    private int linearSearch(T[] objects, T object, int from, int to) {
+        int index = -1;
+        for (int i = from; i <= to; i++) {
+            if (object.equals(objects[i])) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    public static <T> int search(T[] objects, T object) {
         ForkJoinPool forkJoinPool = new ForkJoinPool();
         return forkJoinPool.<Integer>invoke(new ParallelSearch<>(objects, object, 0, objects.length - 1));
     }
